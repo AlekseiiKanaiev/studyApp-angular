b4a9d0cb16fc558d9f588ebb7f01de0d88e86588
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-08-28 18:37:07 +0300

    add contentChild component

--- src/app/basic/basic.module.ts
+++ src/app/basic/basic.module.ts
@@ -10,6 +10,7 @@ import { LifeCycleComponent } from './lifecycle/lifecycle.component';
 import { TemplateVariablesComponent } from './template-var/template-var.component';
 import { TempateVarsChildComponent } from './template-var/temlate-var.child/template-var.child.component';
 import { ViewChildComponent } from './template-var/viewChild/viewChild.component';
+import { ContentChildComponent } from './template-var/contentChild/content.child.component';
 
 @NgModule({
     imports: [
@@ -23,7 +24,8 @@ import { ViewChildComponent } from './template-var/viewChild/viewChild.component
         LifeCycleComponent,
         TemplateVariablesComponent,
         TempateVarsChildComponent,
-        ViewChildComponent
+        ViewChildComponent,
+        ContentChildComponent
     ],
 
 })
--- src/app/basic/template-var/template-var.component.ts
+++ src/app/basic/template-var/template-var.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ViewChildComponent } from './viewChild/viewChild.component';
 
 @Component({
@@ -9,8 +9,36 @@ import { ViewChildComponent } from './viewChild/viewChild.component';
 export class TemplateVariablesComponent {
     name = 'Tom';
 
+    /**С помощью применения к нему декоратора ViewChild к
+     * свойству counterComponent мы устанавливаем, что это
+     * свойство будет содержать объект дочернего компонента,
+     * который внедряется через элемент
+     * <app-viewchild-comp></app-viewchild-comp>.
+     * И в этом случае мы уже можем не использовать шаблонные
+     * переменные в шаблоне. */
     @ViewChild(ViewChildComponent)
     private counterComponent: ViewChildComponent;
+
+    /**Несмотря на то, что выше мы не использовали переменные,
+     * тем не менее с помощью декоратора ViewChild также можно
+     * связать свойство и переменную из шаблона. */
+
+    /**B шаблоне определяется переменная nameText,
+     * которая представляет код параграфа.
+     * А в декоратор ViewChild передается имя этой переменной.
+     * Поэтому свойство nameParagraph, к которому применяется
+     * декоратор, будет указывать на эту переменную nameText.
+     * Причем свойство nameParagraph представляет тип
+     * ElementRef, который используется для ссылки на
+     * элементы html. */
+    @ViewChild('textName')
+    nameParagraph: ElementRef;
+    /**По нажатию на кнопку выводится и изменяется текстовое
+     * содержимое этой переменной. */
+    change() {
+        console.log(this.nameParagraph.nativeElement.textContent);
+        this.nameParagraph.nativeElement.textContent = `Mr. ${this.name}`;
+    }
 
     increment() {
         this.counterComponent.increment();
