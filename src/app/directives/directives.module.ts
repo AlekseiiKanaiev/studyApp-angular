76500de26dde2d9396980c74dcc8a5464fefae87
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-08-29 19:36:48 +0300

    add attributive-directives: ngClass and ngStyle

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
+// Для работы с ngClass  и тп
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { DirectivesRouteModule } from './directives.route.module';
 
 import { DirevtivesComponent } from './directives.component';
+import { AttributiveDirectivesComponent } from './attributive/attributive.directives.component';
 
 
 @NgModule({
     imports: [
+        BrowserModule,
         FormsModule,
         DirectivesRouteModule
     ],
     declarations: [
-        DirevtivesComponent
+        DirevtivesComponent,
+        AttributiveDirectivesComponent
     ]
 })
 export class DirectivesModule {}
